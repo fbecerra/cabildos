ef0db4cae467a7acd430183968b31301ca5216af
Fernando Becerra <becerrafernando@gmail.com>
2022-02-21 23:41:14 -0300

    add arched labels

--- js/cabildos.js
+++ js/cabildos.js
@@ -9,12 +9,13 @@ Promise.all([d3.json("data/cabildos.json")]).then(function(data){
 
   let format = d3.format(",d");
 
-  const width = 932,
+  const width = 800,
         height = width;
+  const charSize = 12;
 
   let pack = data => d3.pack()
     .size([width, height])
-    .padding(0)
+    .padding(charSize)
   (d3.hierarchy(data)
     .sum(d => d.porcentaje)
     .sort((a, b) => b.porcentaje - a.porcentaje));
@@ -28,7 +29,7 @@ Promise.all([d3.json("data/cabildos.json")]).then(function(data){
       .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
       .style("display", "block")
       .style("margin", "0 -14px")
-      .style("background", color(0))
+      // .style("background", color(0))
       .style("cursor", "pointer")
       .on("click", (event) => zoom(event, root));
 
@@ -36,21 +37,59 @@ Promise.all([d3.json("data/cabildos.json")]).then(function(data){
     .selectAll("circle")
     .data(root.descendants().slice(1))
     .join("circle")
-      .attr("fill", d => d.children ? color(d.depth) : "white")
+      .attr('stroke', d => d.children ? 'lightgrey' : color(d.data.comision))
+      .attr('stroke-width', 1.0)
+      .attr("fill", d => d.children ? "white" : color(d.data.comision))
       .attr("pointer-events", d => !d.children ? "none" : null)
       .on("mouseover", function() { d3.select(this).attr("stroke", "#000"); })
       .on("mouseout", function() { d3.select(this).attr("stroke", null); })
       .on("click", (event, d) => focus !== d && (zoom(event, d), event.stopPropagation()));
+
+    const startAngle = -Math.PI / 4;
+  	let labelArc = d3.arc();
+
+		labelArc.innerRadius(function(d) { return (d.r); })
+				.outerRadius(function(d) { return (d.r); })
+				.startAngle(startAngle)
+				.endAngle(function(d) {
+					const total = d.data.name.length;
+					const step = charSize / d.r;
+					return startAngle + (total * step);
+				});
+
+  	const groupLabels = svg
+  			.selectAll(".group")
+				.data(root.descendants().slice(1).filter(function(d) { return d.children; })).enter()
+			.append("g")
+				.attr("class", "group")
+					.attr("transform", function(d) { return `translate(${d.x},${d.y})`; });
+
+    groupLabels
+  		.append("path")
+  			.attr("class", "group-arc")
+  			.attr("id", function(d,i) { return `arc${i}`; })
+  			.attr("d", labelArc);
+
+  	groupLabels
+  		.append("text")
+  			.attr("class", "group-label")
+        .attr("font-size", charSize)
+        .attr("fill", "lightgrey")
+  			.attr("x", 0)
+  			.attr("dy", charSize * 1.5)
+  		.append("textPath")
+  			.attr("xlink:href", function(d,i){ return `#arc${i}`;})
+  			.text(function(d) { return d.data.name ;});
 
   const label = svg.append("g")
       .style("font", "10px sans-serif")
       .attr("pointer-events", "none")
       .attr("text-anchor", "middle")
     .selectAll("text")
-    .data(root.descendants())
+    .data(root.descendants().slice(1).filter(function(d) { return !d.children; }))
     .join("text")
-      .style("fill-opacity", d => d.parent === root ? 1 : 0)
-      .style("display", d => d.parent === root ? "inline" : "none")
+      .style("fill-opacity", 1)
+      .style("display", "inline")
       .text(d => d.data.name);
 
   zoomTo([root.x, root.y, root.r * 2]);
@@ -60,6 +99,11 @@ Promise.all([d3.json("data/cabildos.json")]).then(function(data){
 
     view = v;
 
+    labelArc.innerRadius(d => d.r * k)
+			.outerRadius(d => d.r * k);
+    groupLabels.selectAll(".group-arc")
+  			.attr("d", labelArc)
+    groupLabels.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
     label.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
     node.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
     node.attr("r", d => d.r * k);
@@ -77,11 +121,11 @@ Promise.all([d3.json("data/cabildos.json")]).then(function(data){
           return t => zoomTo(i(t));
         });
 
-    label
-      .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
-      .transition(transition)
-        .style("fill-opacity", d => d.parent === focus ? 1 : 0)
-        .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
-        .on("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
+    // label
+    //   .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
+    //   .transition(transition)
+    //     .style("fill-opacity", d => d.parent === focus ? 1 : 0)
+    //     .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
+    //     .on("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
   }
 })
