55f4a4c7506b28c907cc3aba5a88114e7c5e15f4
Fernando Becerra <becerrafernando@gmail.com>
2022-02-23 12:47:12 -0300

    use horizontal arrangment for circles

--- js/cabildos.js
+++ js/cabildos.js
@@ -5,6 +5,8 @@ state = {
 }
 const transitionTime = 500;
 
+const container = d3.select("#cabildos");
+
 Promise.all([d3.json("data/cabildos.json")]).then(function(data){
   let cabildos = data[0];
   console.log(cabildos);
@@ -145,10 +147,6 @@ Promise.all([d3.json("data/cabildos.json")]).then(function(data){
     label.transition()
       .duration(transitionTime)
       .attr("opacity", calculateOpacity);
-
-    // groupLabels.transition()
-    //   .duration(transitionTime)
-    //   .attr("opacity", calculateOpacity);
   }
 
 
@@ -159,9 +157,9 @@ Promise.all([d3.json("data/cabildos.json")]).then(function(data){
 
   let format = d3.format(",d");
 
-  const width = 800,
-        height = width;
-  const charSize = 12;
+  const height = 700,
+        width = height;
+  const charSize = 2;
 
   let pack = data => d3.pack()
     .size([width, height])
@@ -174,110 +172,61 @@ Promise.all([d3.json("data/cabildos.json")]).then(function(data){
   console.log(root)
   let focus = root;
   let view;
+  const svgWidth = 3/4 * width;
 
   const svg = d3.select("#cabildos").append("svg")
-      .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
-      .style("display", "block")
-      .style("margin", "0 -14px")
-      // .style("background", color(0))
-      .style("cursor", "pointer")
-      .on("click", (event) => zoom(event, root));
+      .attr("viewBox", [0, 0, width, height])
+      .attr("width", svgWidth)
+      .attr("height", height)
+      .attr("style", "max-width: 100%; height: auto; height: intrinsic;")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 10)
+      .attr("text-anchor", "middle")
+      .attr("transform", `rotate(90) scale(1.6)`);
+
+  const offset = (height - svgWidth) / 2;
 
   const node = svg.append("g")
+    .style("transform", `translate(-40px, -${offset}px)`)
     .selectAll("circle")
     .data(root.descendants().slice(1))
     .join("circle")
-      .attr('stroke', d => d.children ? 'lightgrey' : color(d.data.comision))
+      .attr('stroke', d => d.depth === 1 ? 'lightgrey' : color(d.data.comision))
       .attr('stroke-width', 1.0)
-      .attr("fill", d => d.children ? "#E9ECEF" : color(d.data.comision))
+      .attr("fill", d => d.children ? "white" : color(d.data.comision))
       .attr("pointer-events", d => !d.children ? "none" : null)
-      .on("mouseover", function() { d3.select(this).attr("stroke", "#000"); })
-      .on("mouseout", function() { d3.select(this).attr("stroke", "lightgrey"); })
-      .on("click", (event, d) => focus !== d && (zoom(event, d), event.stopPropagation()));
+      .attr("cx", d => d.x)
+      .attr("cy", d => d.y)
+      .attr("r", d => d.r)
+      // .on("mouseover", function() { d3.select(this).attr("stroke", "#000"); })
+      // .on("mouseout", function() { d3.select(this).attr("stroke", "lightgrey"); })
 
-    const startAngle = -Math.PI / 4;
-  	let labelArc = d3.arc();
-
-		labelArc.innerRadius(function(d) { return (d.r); })
-				.outerRadius(function(d) { return (d.r); })
-				.startAngle(startAngle)
-				.endAngle(function(d) {
-					const total = d.data.name.length;
-					const step = charSize / d.r;
-					return startAngle + (total * step);
-				});
-
-  	// const groupLabels = svg
-  	// 		.selectAll(".group")
-		// 		.data(root.descendants().slice(1).filter(function(d) { return d.children; })).enter()
-		// 	.append("g")
-		// 		.attr("class", "group")
-		// 			.attr("transform", function(d) { return `translate(${d.x},${d.y})`; });
+    // const startAngle = -Math.PI / 4;
+  	// let labelArc = d3.arc();
     //
-    // groupLabels
-  	// 	.append("path")
-  	// 		.attr("class", "group-arc")
-  	// 		.attr("id", function(d,i) { return `arc${i}`; })
-  	// 		.attr("d", labelArc);
-    //
-  	// groupLabels
-  	// 	.append("text")
-  	// 		.attr("class", "group-label")
-    //     .attr("font-size", charSize)
-    //     .attr("fill", "lightgrey")
-  	// 		.attr("x", 0)
-  	// 		.attr("dy", charSize * 1.5)
-  	// 	.append("textPath")
-  	// 		.attr("xlink:href", function(d,i){ return `#arc${i}`;})
-  	// 		.text(function(d) { return d.data.name ;});
+		// labelArc.innerRadius(function(d) { return (d.r); })
+		// 		.outerRadius(function(d) { return (d.r); })
+		// 		.startAngle(startAngle)
+		// 		.endAngle(function(d) {
+		// 			const total = d.data.name.length;
+		// 			const step = charSize / d.r;
+		// 			return startAngle + (total * step);
+		// 		});
 
   const label = svg.append("g")
       .style("font", "10px sans-serif")
       .attr("pointer-events", "none")
       .attr("text-anchor", "middle")
+      .style("transform", `translate(0, -${offset}px)`)
+      // .style("transform", "rotate(90)")
     .selectAll("text")
     .data(root.descendants().slice(1).filter(function(d) { return !d.children; }))
     .join("text")
+      .attr("x", d => d.x)
+      .attr("y", d => d.y)
       .style("fill-opacity", 1)
       .style("display", "inline")
-      .text(d => d.data.name.slice(0, 10));
+      // .text(d => d.data.name.slice(0, 10))
+      // .attr("transform", "rotate(0.1)")
 
-  zoomTo([root.x, root.y, root.r * 2]);
-
-  function zoomTo(v) {
-    const k = width / v[2];
-
-    view = v;
-
-    labelArc.innerRadius(d => d.r * k)
-			.outerRadius(d => d.r * k);
-    // groupLabels.selectAll(".group-arc")
-  	// 		.attr("d", labelArc)
-    // groupLabels.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
-    label.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k}) rotate(-90)`);
-    node.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
-    node.attr("r", d => d.r * k);
-
-    svg.attr("transform", "rotate(90)")
-  }
-
-  function zoom(event, d) {
-    const focus0 = focus;
-
-    focus = d;
-
-    const transition = svg.transition()
-        .duration(event.altKey ? 7500 : 750)
-        .tween("zoom", d => {
-          const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
-          return t => zoomTo(i(t));
-        });
-
-    // label
-    //   .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
-    //   .transition(transition)
-    //     .style("fill-opacity", d => d.parent === focus ? 1 : 0)
-    //     .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
-    //     .on("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
-  }
 })
